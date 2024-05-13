0f33c7a0486d631a283e8c80b751b8f0480ebd44
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-13 20:07:50 +0700

    minor

--- index.mjs
+++ index.mjs
@@ -6,7 +6,18 @@ import { AjaxRenderer } from './src/renderer/AjaxRenderer.mjs';
 import { _$ } from './src/utils/_$.mjs';
 import { __ProgressBar } from './src/utils/__ProgressBar.mjs';
 import { __AOnLoadings } from './src/utils/__AOnLoadings.mjs';
+import { _RollupSettings } from './builder/_RollupSettings.mjs';
+import { _RollupTarget } from './builder/_RollupTarget.mjs';
 
 /** @typedef {import('./src/utils/__AOnLoadings.mjs').__AOnLoadings_method} __AOnLoadings_method */
 
-export { __atlaAS_client, __AppSettings, AjaxRenderer, _$, __ProgressBar, __AOnLoadings };
+export {
+	__atlaAS_client,
+	__AppSettings,
+	AjaxRenderer,
+	_$,
+	__ProgressBar,
+	__AOnLoadings,
+	_RollupSettings,
+	_RollupTarget,
+};
