3716a181538df36cdb68504205b7f1318d43a0af
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-05-15 01:42:00 +0700

    test

--- index.mjs
+++ index.mjs
@@ -5,17 +5,7 @@ import { __AppSettings } from './src/vars/__AppSettings.mjs';
 import { AjaxRenderer } from './src/renderer/AjaxRenderer.mjs';
 import { __ProgressBar } from './src/utils/__ProgressBar.mjs';
 import { __AOnLoadings } from './src/utils/__AOnLoadings.mjs';
-import { _RollupSettings } from './builder/_RollupSettings.mjs';
-import { _RollupTarget } from './builder/_RollupTarget.mjs';
 
 /** @typedef {import('./src/utils/__AOnLoadings.mjs').__AOnLoadings_method} __AOnLoadings_method */
 
-export {
-	__atlaAS_client,
-	__AppSettings,
-	AjaxRenderer,
-	__ProgressBar,
-	__AOnLoadings,
-	_RollupSettings,
-	_RollupTarget,
-};
+export { __atlaAS_client, __AppSettings, AjaxRenderer, __ProgressBar, __AOnLoadings };
